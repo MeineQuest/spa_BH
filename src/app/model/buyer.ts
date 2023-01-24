004c441f2387918b7df31a33bc54f8c1a0fa2656
MeineQuest <Code@MeineQuest.de>
2023-01-24 21:08:11 +0100

    fixed models

--- src/app/model/buyer.ts
+++ src/app/model/buyer.ts
@@ -1,6 +1,5 @@
 import { Person } from "./person";
 
 export interface Buyer extends Person{
-
     link:string;
 }
--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -1,8 +1,4 @@
 import { Order } from "./order";
 import { Person } from "./person";
 
-
-export interface Customer extends Person {
-    orders: Order[];
-
-}
+export interface Customer extends Person {}
