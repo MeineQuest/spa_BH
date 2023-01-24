75340015ed8e8959a6f64f3418924c527b63bcd6
MeineQuest <Code@MeineQuest.de>
2023-01-24 21:11:08 +0100

    app.component changed for nav and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { OrderService } from './service/order.service';
+import { PeopleService } from './service/people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'spa_BH';
+  showFiller = false;
+
+  private orderService = inject(OrderService);
+  private peopleService = inject(PeopleService);
+
+  ngOnInit(): void {
+    this.orderService.loadFromLocalStorage();
+    this.peopleService.loadBuyersFromLocalStorage();
+    this.peopleService.loadCustomersFromLocalStorage();
+  }
+
 }
