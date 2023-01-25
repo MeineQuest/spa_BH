df6c5c50d1a4b179ea9112f3761e94fd560d357f
MeineQuest <Code@MeineQuest.de>
2023-01-25 21:47:23 +0100

    added routing and deleted example code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleOverviewComponent } from './components/peopleoverview/peopleoverview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'overview',
+    component: PeopleOverviewComponent
+  },
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full'
+
+  },
+  {
+    path: '**',
+    component: PeopleOverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
